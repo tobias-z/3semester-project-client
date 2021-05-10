653824f5997e115549061cc36669d66ab87cb5e3
Rasmus Hansen <rasmush22@live.dk>
2021-05-10 15:05:39 +0200

    feat: make notification for add to basket buttons

--- src/routes/restaurant/Restaurant.js
+++ src/routes/restaurant/Restaurant.js
@@ -1,23 +1,38 @@
-import * as React from "react"
-import {Button, Card} from "react-bootstrap"
-import {useParams} from "react-router-dom"
-import {sortRestaurants} from "./RestaurantsPage"
+import * as React from "react";
+import { Button, Card, Toast } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { sortRestaurants } from "./RestaurantsPage";
 
 function Restaurant(props) {
-  const {restaurantDATA} = props
-  let {name} = useParams()
-  const [restaurant, setRestaurant] = React.useState()
+  const { restaurantDATA } = props;
+  let { name } = useParams();
+
+  const [restaurant, setRestaurant] = React.useState();
+  const [show, setShow] = React.useState(false);
+  const [chosenMenu, setChosenMenu] = React.useState({
+    menu: "",
+    itemCount: "",
+  });
+
+  const toggleShow = () => setShow(true);
+
   React.useEffect(() => {
-    const tempRestaurant = restaurantDATA.restaurants.find(restaurant => {
-      if (restaurant.name === name) return true
-      return false
-    })
-    setRestaurant(tempRestaurant)
-  }, [name, restaurantDATA.restaurants])
-  let lastCategory
+    const tempRestaurant = restaurantDATA.restaurants.find((restaurant) => {
+      if (restaurant.name === name) return true;
+      return false;
+    });
+    setRestaurant(tempRestaurant);
+  }, [name, restaurantDATA.restaurants]);
+  let lastCategory;
 
   return (
     <div>
+      <NotificationPopUp
+        show={show}
+        menu={chosenMenu.menu}
+        itemCount={chosenMenu.itemCount}
+        setShow={setShow}
+      />
       {restaurant && (
         <>
           <div className="text-center">
@@ -25,35 +40,42 @@ function Restaurant(props) {
             <h4>{restaurant.description}</h4>
           </div>
           <div className="mt-5">
-            {restaurant.menus.sort(sortRestaurants).map(menu => {
-              let isNewCategory = false
+            {restaurant.menus.sort(sortRestaurants).map((menu) => {
+              let isNewCategory = false;
               if (lastCategory !== menu.category) {
-                isNewCategory = true
-                lastCategory = menu.category
+                isNewCategory = true;
+                lastCategory = menu.category;
               }
               return (
                 <MenuItem
                   key={menu.id}
                   menu={menu}
                   isNewCategory={isNewCategory}
+                  setChosenMenu={setChosenMenu}
+                  toggleShow={toggleShow}
                 />
-              )
+              );
             })}
           </div>
         </>
       )}
     </div>
-  )
+  );
 }
 
 function MenuItem(props) {
-  const {isNewCategory, menu} = props
-  const [itemCount, setItemCount] = React.useState(1)
-  const isCountOne = itemCount === 1
+  const { isNewCategory, menu, setChosenMenu, toggleShow } = props;
+  const [itemCount, setItemCount] = React.useState(1);
+  const isCountOne = itemCount === 1;
 
-  const increment = () => setItemCount(itemCount + 1)
-  const decriment = () => setItemCount(itemCount - 1)
+  const increment = () => setItemCount(itemCount + 1);
+  const decriment = () => setItemCount(itemCount - 1);
 
+  function handleInputBasket() {
+    // put setChosenMenu and toggleShow into .then for our fetch function when the endpoint is ready
+    setChosenMenu({ menu, itemCount });
+    toggleShow();
+  }
   function generateCardBody() {
     return (
       <Card.Body>
@@ -61,31 +83,38 @@ function MenuItem(props) {
         <Card.Text>{menu.description}</Card.Text>
         <div className="d-flex">
           <Button
-            style={{borderRadius: 0}}
+            style={{ borderRadius: 0 }}
             variant="outline-secondary"
             disabled={isCountOne}
-            onClick={decriment}>
+            onClick={decriment}
+          >
             -
           </Button>
           <Button
-            style={{borderRadius: 0}}
+            style={{ borderRadius: 0 }}
             disabled
             className="px-3"
-            variant="outline-secondary">
+            variant="outline-secondary"
+          >
             {itemCount}
           </Button>
           <Button
-            style={{borderRadius: 0}}
+            style={{ borderRadius: 0 }}
             variant="outline-secondary"
-            onClick={increment}>
+            onClick={increment}
+          >
             +
           </Button>
-          <Button className="ml-3 w-50" variant="secondary">
+          <Button
+            className="ml-3 w-50"
+            variant="secondary"
+            onClick={handleInputBasket}
+          >
             {menu.price * itemCount}, - €
           </Button>
         </div>
       </Card.Body>
-    )
+    );
   }
 
   if (isNewCategory) {
@@ -99,10 +128,27 @@ function MenuItem(props) {
         </div>
         <Card className="my-2">{generateCardBody()}</Card>
       </>
-    )
+    );
   } else {
-    return <Card className="my-2">{generateCardBody()}</Card>
+    return <Card className="my-2">{generateCardBody()}</Card>;
   }
 }
 
-export default Restaurant
+function NotificationPopUp(props) {
+  const { show, menu, itemCount, setShow } = props;
+
+  return (
+    <Toast show={show} onClose={() => setShow(false)} className="popup">
+      <Toast.Header>
+        <strong className="mr-auto">Product: {menu.itemName} </strong>
+        <small>Just now!</small>
+      </Toast.Header>
+
+      <Toast.Body>
+        Amount: {itemCount} - Price: {menu.price * itemCount}
+      </Toast.Body>
+    </Toast>
+  );
+}
+
+export default Restaurant;
